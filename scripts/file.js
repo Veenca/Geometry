8acaed0753cceca9350cb255eb97d3e9c4d6da9f
Veenca <63163300+Veenca@users.noreply.github.com>
2020-09-03 16:01:14 +0200

    'update'

--- scripts/file.js
+++ scripts/file.js
@@ -2,6 +2,8 @@
 let figureInput = document.getElementById('inputFigure');
 let btn = document.getElementById('btn');
 let useless = document.getElementById('height_span');
+undobtn=document.getElementById('undobtn')
+console.log(undobtn);
 let value;
 let options
 let width;
@@ -24,7 +26,7 @@ function onClick() {
 
             console.log(options[i].value);
             beenClicked = true;
-            figureInput.value='';
+            figureInput.value = '';
             break;
         }
     }
@@ -46,37 +48,82 @@ function onInput() {
 }
 figureInput.addEventListener('input', onInput, false);
 btn.addEventListener('click', onClick, false);
-
+let drawings = [];
 function setup() {
     let canvas = createCanvas(document.getElementById('drawingPanel').offsetWidth, document.getElementById('drawingPanel').offsetHeight);
     canvas.parent('drawingPanel');
-    
+
     background(220);
 }
-
+let i = 0;
+let b;
 function draw() {
 
     if (beenClicked === true) {
         fill(255);
-        switch(value){
-            case 'Square':
-                if (mouseIsPressed) {
-                rect(mouseX-(width/2), mouseY-(width/2), width, width);
-                }
-            break;
-            case 'Rectangle':
-                if (mouseIsPressed) {
-                    rect(mouseX, mouseY, width, height);
-                    }
-            break;
-            case 'Ellipse':
-                if (mouseIsPressed) {
-                    ellipse(mouseX, mouseY, width, height);
-                    }
-            break;
-        }
-         
+        if (mouseIsPressed) {
+            switch (value) {
+                case 'Square':
 
+                    b = new Rectangle(mouseX - (width / 2), mouseY-(width/2), width, width);
+                    drawings.push(b);
+                    drawings[i].show();
+                    i++;
+                  
+
+                    break;
+                case 'Rectangle':
+
+                    
+                    b = new Rectangle(mouseX - (width / 2), mouseY-(height/2), width, height);
+                    drawings.push(b);
+                    drawings[i].show();
+                    i++;
+                  
+
+                    break;
+                case 'Ellipse':
+
+                    b = new Ellipse(mouseX, mouseY, width, height);
+                    drawings.push(b);
+                    drawings[i].show();
+                    i++;
+                   
+
+                    break;
+            }
+
+        }
+    }
+}
+class Rectangle {
+
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+    show() {
+        stroke(0);
+        fill(225);
+        rect(this.x, this.y, this.w, this.h);
+    }
+}
+class Square {
+
+}
+class Ellipse {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+    show() {
+        stroke(0);
+        fill(225);
+        ellipse(this.x, this.y, this.w, this.h);
     }
 }
 
---
+++ scripts/notgud.js
@@ -0,0 +1,26 @@
+
+/*function draw() {
+
+    if (beenClicked === true) {
+        fill(255);
+        switch(value){
+            case 'Square':
+                if (mouseIsPressed) {
+                rect(mouseX-(width/2), mouseY-(width/2), width, width);
+                }
+            break;
+            case 'Rectangle':
+                if (mouseIsPressed) {
+                    rect(mouseX, mouseY, width, height);
+                    }
+            break;
+            case 'Ellipse':
+                if (mouseIsPressed) {
+                    ellipse(mouseX, mouseY, width, height);
+                    }
+            break;
+        }
+         
+
+    }
+}*/
