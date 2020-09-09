4f869b61d1bc0059729c6bf44b5c1f2db974a996
Vincenzo <63163300+Veenca@users.noreply.github.com>
2020-09-09 13:17:57 +0200

    Add files via upload

---
+++ Play_with_Geometry/scripts/notgud.js
@@ -0,0 +1,48 @@
+
+/*function draw() {
+
+    if (beenClicked === true) {
+        fill(255);
+        switch(value){
+            case 'Square':
+                if (mouseIsPressed) {
+                rect(mouseX-(width/2), mouseY-(width/2), width, width);
+                }
+            break;
+            case 'Rectangle':
+                if (mouseIsPressed) {
+                    rect(mouseX, mouseY, width, height);
+                    }
+            break;
+            case 'Ellipse':
+                if (mouseIsPressed) {
+                    ellipse(mouseX, mouseY, width, height);
+                    }
+            break;
+        }
+         
+
+    }
+}*/
+/* Undo function*
+
+function mousePressed() {
+    
+    if(undo==true){
+       
+    for (let j = drawings.length - 1; j >= 0; j--) {
+        
+      if (drawings[j].contains(mouseX, mouseY)) {
+        
+       drawings.splice(j, 1);
+      
+        console.log(drawings.length);
+        console.log(drawings[j]);
+        i--;
+      }
+    }
+}
+  }
+
+*/
+
