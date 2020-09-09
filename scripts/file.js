b540a69653518af766683b3423ede5eff7e0db81
Veenca <63163300+Veenca@users.noreply.github.com>
2020-09-09 13:14:26 +0200

    update

--- scripts/file.js
+++ scripts/file.js
@@ -3,7 +3,8 @@ let figureInput = document.getElementById('figureSelection');
 let useless = document.getElementById('height_span');
 undobtn = document.getElementById('undobtn')
 let undo = false;
-
+let beenClicked = false;
+goBackBtn = document.getElementById('goBackBtn');
 function checkValue(radios) {
 
     for (let j = 0, length = radios.length; j < length; j++) {
@@ -36,42 +37,53 @@ function undoelmt() {
     if (undo == false) {
         undo = true;
         undobtn.setAttribute('class', 'clicked');
-       
+
     } else if (undo == true) {
         undo = false;
         undobtn.removeAttribute('class');
     }
-    console.log(undo);
-    
-}
+    //   console.log(undo); //DEBUG
 
+}
+let drawings = [];
 figureInput.addEventListener('click', onInput, false);
 
 undobtn.addEventListener('click', undoelmt, false);
+goBackBtn.addEventListener('click', goback, false);
+function goback() {
+    if (drawings.length >= 0) {
+        drawings.pop();
+    }
+
+}
+
 let div = document.getElementById('drawingPanel');
 
-let drawings = [];
+
 let canvas;
 function setup() {
     canvas = createCanvas(div.offsetWidth, div.offsetHeight);
     canvas.parent('drawingPanel');
-
 
 }
 let i = 0;
 let b;
 let x;
 let y;
-function mousePressed() {
+let fg_Red=document.getElementById('fg_RedValue');
+let fg_Green=document.getElementById('fg_GreenValue');
+let fg_Blue=document.getElementById('fg_BlueValue');
 
+function mousePressed() {
+  
     var e = window.event;
     var posX = e.clientX;
     var posY = e.clientY;
     let isclicking = findpoint((div.offsetWidth + div.offsetLeft), div.offsetLeft, div.offsetTop, (div.offsetHeight + div.offsetTop), posX, posY);
 
 
-    if (undo == false && isclicking == true) {
-        let c = color(document.getElementById('fg_RedValue').value, document.getElementById('fg_GreenValue').value, document.getElementById('fg_BlueValue').value);
+    if (undo == false && isclicking == true && beenClicked == false) {
+      let c= color(fg_Red.value, fg_Green.value,fg_Blue.value);
 
         switch (checkValue(document.getElementsByName('figure'))) {
             case 'Square':
@@ -85,20 +97,28 @@ function mousePressed() {
                 break;
         }
         drawings.push(b);
-        console.log(drawings.length)
-        
+        //    console.log(drawings.length)// DEBUG
 
-    }if(undo==true && isclicking == true){
-        console.log('ok');
+
+    } else if (undo == true && isclicking == true) {
+        //  console.log('ok'); // DEBUG
+        for (let i = 0; i < drawings.length; i++) {
+
+            if (drawings[i].contains(mouseX, mouseY)) {
+                drawings.splice(i, 1);
+                //      console.log(drawings.length); //DEBUG
+            }
+
+
+        }
     }
-    
-
 }
+
 
 function draw() {
 
     background(document.getElementById('bg_RedValue').value, document.getElementById('bg_GreenValue').value, document.getElementById('bg_BlueValue').value);
-    //let asd=checkValue(document.getElementsByName('figure'));
+
 
     for (i = 0; i < drawings.length; i++) {
         drawings[i].show();
@@ -106,22 +126,30 @@ function draw() {
 
     }
 
-    let c = color(document.getElementById('fg_RedValue').value, document.getElementById('fg_GreenValue').value, document.getElementById('fg_BlueValue').value);
+
+   
+    let c = color(fg_Red.value, fg_Green.value,fg_Blue.value);
+    
     fill(c);
     div.style.cursor = 'pointer';
-    switch (checkValue(document.getElementsByName('figure'))) {
-        case 'Square':
+    if (undo == false && beenClicked==false) {
+        switch (checkValue(document.getElementsByName('figure'))) {
+            case 'Square':
 
-            rect(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('baseValue').value / 2), document.getElementById('baseValue').value, document.getElementById('baseValue').value);
-            break;
-        case 'Rectangle':
-            rect(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('heightValue').value / 2), document.getElementById('baseValue').value, document.getElementById('heightValue').value);
-            break;
-        case 'Ellipse':
-            ellipse(mouseX, mouseY, document.getElementById('baseValue').value, document.getElementById('heightValue').value);
-            break;
+                rect(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('baseValue').value / 2), document.getElementById('baseValue').value, document.getElementById('baseValue').value);
+                break;
+            case 'Rectangle':
+                rect(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('heightValue').value / 2), document.getElementById('baseValue').value, document.getElementById('heightValue').value);
+                break;
+            case 'Ellipse':
+                ellipse(mouseX, mouseY, document.getElementById('baseValue').value, document.getElementById('heightValue').value);
+                break;
+        }
+
     }
-
+    if (undo == true) {
+        div.style.cursor = 'crosshair';
+    }
 }
 class Rectangle {
 
@@ -136,6 +164,17 @@ class Rectangle {
         stroke(0);
         fill(this.color);
         rect(this.x, this.y, this.w, this.h);
+    }
+    contains(px, py) {
+
+        let offw = (this.w * 1) + this.x;
+        let offleft = this.x;
+        let offh = (this.h * 1) + this.y;
+        let offtop = this.y;
+
+        return findpoint(offw, offleft, offtop, offh, px, py);
+
+
     }
 
 
@@ -154,10 +193,14 @@ class Ellipse {
         fill(this.color);
         ellipse(this.x, this.y, this.w, this.h);
     }
-
+    contains(px, py) {
+        //  console.log(this.x,this.y,this.w,this.h,px,py);
+        return findPointEllipse(this.x, this.y, this.w, this.h, px, py);
+    }
 }
 
 function findpoint(x1, x2, y1, y2, x, y) {
+
 
     if (x <= x1 && x >= x2 &&
         y >= y1 && y <= y2) {
@@ -168,3 +211,55 @@ function findpoint(x1, x2, y1, y2, x, y) {
         return false;
     }
 }
+
+function findPointEllipse(h, k, a, b, x, y) {
+    let f1 = Math.pow((x - h), 2);
+    let f2 = Math.pow((a / 2), 2);
+    let f3 = Math.pow((y - k), 2);
+    let f4 = Math.pow((b / 2), 2);
+
+    let p = (f1 / f2) + (f3 / f4);
+    if (p <= 1) {
+        return true;
+    } else { return false; }
+
+}
+
+// Simple example, see optional options for more configuration.
+const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'classic', // or 'monolith', or 'nano'
+    default: 'rgb(225, 225, 225)',
+   
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: false,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: false,
+            rgba: false,
+            hsla: false,
+            hsva: false,
+            cmyk: false,
+            input: true,
+            clear: false,
+            save: true
+        }
+    }
+});
+pickr.on('show', (color, instance) => {
+  //  console.log('show', color, instance);
+    beenClicked=true;
+}).on('save', (color, instance) => {
+    console.log(color.toRGBA());
+    pickr.hide();
+    beenClicked=false;
+    fg_Blue.value=color.toRGBA()[2];
+    fg_Green.value=color.toRGBA()[1];
+    fg_Red.value=color.toRGBA()[0];
+});
