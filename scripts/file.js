7316282eadbd33f256bc18b86aac0be23e232609
Vincenzo <63163300+Veenca@users.noreply.github.com>
2020-09-10 15:25:46 +0200

    Added: -color palette

--- scripts/file.js
+++ scripts/file.js
@@ -5,7 +5,7 @@ undobtn = document.getElementById('undobtn')
 let undo = false;
 let beenClicked = false;
 goBackBtn = document.getElementById('goBackBtn');
-let savebtn= document.getElementById('Save');
+let savebtn = document.getElementById('Save');
 function checkValue(radios) {
 
     for (let j = 0, length = radios.length; j < length; j++) {
@@ -69,14 +69,13 @@ function setup() {
 }
 let i = 0;
 let b;
-let x;
-let y;
-let fg_Red=document.getElementById('fg_RedValue');
-let fg_Green=document.getElementById('fg_GreenValue');
-let fg_Blue=document.getElementById('fg_BlueValue');
+
+let fg_Red = document.getElementById('fg_RedValue');
+let fg_Green = document.getElementById('fg_GreenValue');
+let fg_Blue = document.getElementById('fg_BlueValue');
 
 function mousePressed() {
-  
+
     var e = window.event;
     var posX = e.clientX;
     var posY = e.clientY;
@@ -84,35 +83,36 @@ function mousePressed() {
 
 
     if (undo == false && isclicking == true && beenClicked == false) {
-      let c= color(fg_Red.value, fg_Green.value,fg_Blue.value);
-
+        let c = color(fg_Red.value, fg_Green.value, fg_Blue.value);
+        let sV = document.getElementById('strokeValue').value;
+        console.log(sV);
         switch (checkValue(document.getElementsByName('figure'))) {
             case 'Square':
-                b = new Rectangle(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('baseValue').value / 2), document.getElementById('baseValue').value, document.getElementById('baseValue').value, c);
+                b = new Rectangle(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('baseValue').value / 2), document.getElementById('baseValue').value, document.getElementById('baseValue').value, c, sV);
                 break;
             case 'Rectangle':
-                b = new Rectangle(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('heightValue').value / 2), document.getElementById('baseValue').value, document.getElementById('heightValue').value, c);
+                b = new Rectangle(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('heightValue').value / 2), document.getElementById('baseValue').value, document.getElementById('heightValue').value, c, sV);
                 break;
             case 'Ellipse':
-                b = new Ellipse(mouseX, mouseY, document.getElementById('baseValue').value, document.getElementById('heightValue').value, c);
+                b = new Ellipse(mouseX, mouseY, document.getElementById('baseValue').value, document.getElementById('heightValue').value, c, sV);
                 break;
         }
         drawings.push(b);
         //    console.log(drawings.length)// DEBUG
 
-    }else if(undo==true && isclicking == true){
+    } else if (undo == true && isclicking == true) {
         console.log('ok');
-        for(let i=0;i<drawings.length;i++){
-         if(drawings[i].contains(mouseX,mouseY)){
-            drawings.splice(i,1);
-             
-         }
-        
-         
-           } 
+        for (let i = 0; i < drawings.length; i++) {
+            if (drawings[i].contains(mouseX, mouseY)) {
+                drawings.splice(i, 1);
+
+            }
+
+
+        }
     }
 }
-    
+
 
 
 
@@ -129,56 +129,60 @@ function draw() {
     }
 
 
-   
-    let c = color(fg_Red.value, fg_Green.value,fg_Blue.value);
-    
+
+    let c = color(fg_Red.value, fg_Green.value, fg_Blue.value);
+    let sV = document.getElementById('strokeValue').value;
     fill(c);
     div.style.cursor = 'pointer';
-    if(undo==false){
-    switch (checkValue(document.getElementsByName('figure'))) {
-        case 'Square':
+    if (undo == false) {
+        switch (checkValue(document.getElementsByName('figure'))) {
+            case 'Square':
+                strokeWeight(sV);
+                rect(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('baseValue').value / 2), document.getElementById('baseValue').value, document.getElementById('baseValue').value);
+                break;
+            case 'Rectangle':
+                strokeWeight(sV);
+                rect(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('heightValue').value / 2), document.getElementById('baseValue').value, document.getElementById('heightValue').value);
+                break;
+            case 'Ellipse':
+                strokeWeight(sV);
+                ellipse(mouseX, mouseY, document.getElementById('baseValue').value, document.getElementById('heightValue').value);
+                break;
+        }
 
-            rect(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('baseValue').value / 2), document.getElementById('baseValue').value, document.getElementById('baseValue').value);
-            break;
-        case 'Rectangle':
-            rect(mouseX - (document.getElementById('baseValue').value / 2), mouseY - (document.getElementById('heightValue').value / 2), document.getElementById('baseValue').value, document.getElementById('heightValue').value);
-            break;
-        case 'Ellipse':
-            ellipse(mouseX, mouseY, document.getElementById('baseValue').value, document.getElementById('heightValue').value);
-            break;
     }
-    
-}
 
-    
+
     if (undo == true) {
         div.style.cursor = 'crosshair';
     }
 }
 class Rectangle {
 
-    constructor(x, y, w, h, c) {
+    constructor(x, y, w, h, c, sV) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
         this.color = c
+        this.strokeValue = sV;
     }
     show() {
         stroke(0);
+        strokeWeight(this.strokeValue);
         fill(this.color);
         rect(this.x, this.y, this.w, this.h);
     }
-    contains(px,py){
+    contains(px, py) {
 
-        let offw=(this.w*1)+this.x;
-        let offleft=this.x;
-        let offh=(this.h*1)+this.y;
-        let offtop=this.y;
-      
-      return findpoint(offw,offleft,offtop,offh,px,py);
-      
-    
+        let offw = (this.w * 1) + this.x;
+        let offleft = this.x;
+        let offh = (this.h * 1) + this.y;
+        let offtop = this.y;
+
+        return findpoint(offw, offleft, offtop, offh, px, py);
+
+
     }
 
 
@@ -204,7 +208,7 @@ class Ellipse {
 }
 
 function findpoint(x1, x2, y1, y2, x, y) {
-    
+
 
 
     if (x <= x1 && x >= x2 &&
@@ -235,7 +239,7 @@ const pickr = Pickr.create({
     el: '.color-picker',
     theme: 'classic', // or 'monolith', or 'nano'
     default: 'rgb(225, 225, 225)',
-   
+
 
     components: {
 
@@ -258,17 +262,56 @@ const pickr = Pickr.create({
     }
 });
 pickr.on('show', (color, instance) => {
-  //  console.log('show', color, instance);
-    beenClicked=true;
+    //  console.log('show', color, instance);
+    beenClicked = true;
 }).on('save', (color, instance) => {
     console.log(color.toRGBA());
     pickr.hide();
-    beenClicked=false;
-    fg_Blue.value=color.toRGBA()[2];
-    fg_Green.value=color.toRGBA()[1];
-    fg_Red.value=color.toRGBA()[0];
+    beenClicked = false;
+    fg_Blue.value = color.toRGBA()[2];
+    fg_Green.value = color.toRGBA()[1];
+    fg_Red.value = color.toRGBA()[0];
 });
-function save(){
+function save() {
     save(canvas, 'myCanvas.jpg');
 }
-savebtn.addEventListener('click',save,false)
+savebtn.addEventListener('click', save, false)
+let previewDiv = document.getElementById('drawingPreview');
+let s2 = function (sketch) {
+
+    sketch.setup = function () {
+        let canvas2 = sketch.createCanvas(previewDiv.offsetWidth, previewDiv.offsetHeight);
+        canvas2.parent('drawingPreview');
+
+    }
+
+    sketch.draw = function () {
+        //for canvas 2
+        sketch.background(225);
+        let c = color(fg_Red.value, fg_Green.value, fg_Blue.value);
+        sketch.fill(c);
+        let sV = document.getElementById('strokeValue').value;
+        // console.log(checkValue(document.getElementsByName('figure')));
+        switch (checkValue(document.getElementsByName('figure'))) {
+
+            case 'Square':
+
+                sketch.strokeWeight(sV);
+
+                sketch.rect(75, 75, document.getElementById('baseValue').value, document.getElementById('baseValue').value);
+                break;
+            case 'Rectangle':
+                sketch.strokeWeight(sV);
+
+                sketch.rect(1, 1, document.getElementById('baseValue').value, document.getElementById('heightValue').value);
+                break;
+            case 'Ellipse':
+                sketch.strokeWeight(sV);
+                sketch.ellipse(75, 75, document.getElementById('baseValue').value, document.getElementById('heightValue').value);
+                break;
+        }
+    }
+};
+
+// create the second instance of p5 and pass in the function for sketch 2
+new p5(s2);
